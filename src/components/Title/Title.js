8d94768c75efafbda79fe06e8938b72487d3f2ee
manshind <manshind@yandex-team.ru>
2023-10-28 01:35:01 +0300

    fix

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,18 +1,18 @@
-import {createElement} from "react";
-import cn from 'classnames'
-import styles from './Title.module.scss'
-
-const VARIANT_TO_ELEMENT = {
-  primary: 'h1',
-  secondary: 'h1',
-}
+import cn from "classnames";
+import styles from './Title.module.scss';
 
 export function Title(props) {
-  const {className, variant = 'primary', children} = props
+  const {className, width, height, children} = props
 
-  return createElement(
-    VARIANT_TO_ELEMENT[variant],
-    {className: cn(styles.title, styles[variant], className)},
-    children,
+  return (
+    <svg
+      className={cn(styles.title, className)}
+      style={{'--width': `${width}px`, '--height': `${height}px`}}
+      viewBox={`0 0 ${width} ${height}`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
   )
 }

d167edba8835e1be919fdb5c9dc0d0526419a22d
manshind <manshind@yandex-team.ru>
2023-10-31 16:21:43 +0300

    fix

--- src/utils/openVacancies.js
+++ src/utils/openVacancies.js
@@ -1,3 +1,3 @@
 export function openVacancies() {
-  window.open('https://sbergraduate.ru/sberseasons-moscow/', '_blank')
+  window.open('https://sbergraduate.ru/sberseasons-moscow/?utm_source=futuretoday&utm_medium=sber-game&utm_campaign=2023', '_blank')
 }

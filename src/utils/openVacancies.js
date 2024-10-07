8e0a564cee3f2db04574877f272eb33d7083cb99
re1gny <ya.manshin@mail.ru>
2024-10-07 18:12:46 +0300

    fix

--- src/utils/openVacancies.js
+++ src/utils/openVacancies.js
@@ -1,3 +1,3 @@
 export function openVacancies() {
-  window.open('https://sbergraduate.ru/sberseasons-moscow-specproject/#registration', '_blank')
+  window.open('https://sbergraduate.ru/sberseasons-moscow', '_blank')
 }
